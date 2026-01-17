b8e9847a01b16ad1d76f246a6c0d10fdce3e8098
Dolev Hadar <dolevc2@gmail.com>
2026-01-17 14:37:36 +0200

    test: fix

--- internal/tui/cmds_test.go
+++ internal/tui/cmds_test.go
@@ -22,31 +22,6 @@ func TestMergingOfSameWorkflowJobs(t *testing.T) {
 	}
 
 	wfr := makeWorkflowRun(res.Data.Resource.PullRequest.Commits.Nodes[0].Commit.StatusCheckRollup.Contexts.Nodes[0].CheckRun)
-	next := []data.WorkflowRun{
-		{
-			Id:       wfr.Id,
-			Name:     wfr.Name,
-			Link:     wfr.Link,
-			Workflow: wfr.Workflow,
-			Event:    wfr.Event,
-			Jobs: []data.WorkflowJob{
-				{
-					Id:         "job2",
-					State:      api.StatusCompleted,
-					Conclusion: api.ConclusionSuccess,
-					Name:       "job2",
-					Title:      "job2",
-					Workflow:   wfr.Name,
-					Event:      "pull_request",
-					Logs:       []data.LogsWithTime{},
-					Link:       "https://github.com/dlvhdr/gh-dash/actions/runs/19991547923/job/57332991075",
-					Bucket:     data.CheckBucketPass,
-					Kind:       data.JobKindCheckRun,
-				},
-			},
-			Bucket: wfr.Bucket,
-		},
-	}
 
 	m := NewModel("dlvhdr/gh-dash", "1", ModelOpts{})
 	m.prWithChecks = res.Data.Resource.PullRequest
@@ -64,22 +39,49 @@ func TestMergingOfSameWorkflowJobs(t *testing.T) {
 		t.Fatalf(`expected jobs to have length of 1, got: %d`, len(m.workflowRuns[0].Jobs))
 	}
 
+	next := []data.WorkflowRun{
+		{
+			Id:        wfr.Id,
+			Name:      wfr.Name,
+			Link:      wfr.Link,
+			Workflow:  wfr.Workflow,
+			Event:     wfr.Event,
+			RunNumber: wfr.RunNumber,
+			Jobs: []data.WorkflowJob{
+				{
+					RunNumber:  wfr.RunNumber,
+					Id:         "job2",
+					State:      api.StatusCompleted,
+					Conclusion: api.ConclusionSuccess,
+					Name:       "job2",
+					Title:      "job2",
+					Workflow:   wfr.Name,
+					Event:      "pull_request",
+					Logs:       []data.LogsWithTime{},
+					Link:       "https://github.com/dlvhdr/gh-dash/actions/runs/19991547923/job/57332991075",
+					Bucket:     data.CheckBucketPass,
+					Kind:       data.JobKindCheckRun,
+				},
+			},
+			Bucket: wfr.Bucket,
+		},
+	}
 	msg2 := workflowRunsFetchedMsg{runs: next}
 	m.mergeWorkflowRuns(msg2)
 
 	if len(m.workflowRuns) != 1 {
-		t.Errorf(`expected workflow runs to have length of 1, got: %d`, len(m.workflowRuns))
+		t.Fatalf(`expected workflow runs to have length of 1, got: %d, %+v`, len(m.workflowRuns), m.workflowRuns)
 	}
 
 	if len(m.workflowRuns[0].Jobs) != 2 {
-		t.Errorf(`expected jobs to have length of 2, got: %d`, len(m.workflowRuns[0].Jobs))
+		t.Fatalf(`expected jobs to have length of 2, got: %d`, len(m.workflowRuns[0].Jobs))
 	}
 }
 
 func TestMergingOfDifferentWorkflowJobs(t *testing.T) {
 	d, err := os.ReadFile("./testdata/fetchPROneContext.json")
 	if err != nil {
-		t.Errorf("failed reading mock data file %v", err)
+		t.Fatalf("failed reading mock data file %v", err)
 	}
 
 	res := struct{ Data api.PRCheckRunsQuery }{}
